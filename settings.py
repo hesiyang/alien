283cd67f5a2ce7afcdda40b68d9d25bf2aaa31b9
[hesiyang] <hesi2013yang@163.com>
2017-09-23 19:54:00 +0800

    python_alien

---
+++ settings.py
@@ -0,0 +1,9 @@
+#存储外星人入侵游戏的所有设置类
+class Settings():
+    def __init__(self):
+        #屏幕设置
+        self.screen_width = 1200
+        self.screen_height = 800
+        self.bg_color = (230,230,230)
+        self.ship_speed_factor = 1.5
+    
