8daf706997f0bcc84d3c6c9907e7e18dc1600f22
[hesiyang] <hesi2013yang@163.com>
2017-09-24 13:41:02 +0800

    飞船以及开火完成

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,8 +1,10 @@
 import sys
 import pygame
+from pygame.sprite import Group
 import game_functions as gf
 from settings import Settings
 from ship import Ship
+
 
 
 def run_game():
@@ -11,10 +13,13 @@ def run_game():
     screen = pygame.display.set_mode((ai_settings.screen_width,ai_settings.screen_height))
     pygame.display.set_caption("Alien Invasion")
     ship = Ship(ai_settings,screen)
+    bullets = Group()
 
     while True:
-        gf.check_events(ship)    
-        ship.update()    
-        gf.update_screen(ai_settings, screen, ship)
+        gf.check_events(ai_settings,screen,ship,bullets)    
+        ship.update()
+        bullets.update()
+        gf.update_bullets(bullets)
+        gf.update_screen(ai_settings, screen, ship,bullets)
 
 run_game()
